bfd4aa308eed923a97fbe627277379c555129212
Jesse Sandvik <sandvikjesse@gmail.com>
2024-01-28 13:42:53 -0500

    1-28-24: expanding test coverage for heading component

--- src/components/atoms/heading/Heading.test.tsx
+++ src/components/atoms/heading/Heading.test.tsx
@@ -7,12 +7,29 @@ import { HEADING_TYPE, HeadingProps } from "./Heading.types";
 
 describe("[ ATOMS ] Testing Component: '<Heading />'", () => {
 
-  test("[ 1 ] attribute 'className' sets rendered heading 'class' attribute", () => {
-    const text: string = "heading text";
-    const className: HeadingProps["className"] = "test-class";
-    const headingText: HeadingProps["children"] = text;
+    test("[ 1 ] attribute 'children' sets rendered heading text", () => {
+        const text: string = "heading text";
+        const headingText: HeadingProps["children"] = text;
+    
+        render(<Heading children={headingText} type={HEADING_TYPE.H1} />)
+        expect(screen.getByRole("heading", { name: text })).toBeInTheDocument();
+    });
 
-    render(<Heading children={headingText} className={className} type={HEADING_TYPE.H1} />)
-    expect(screen.getByRole("heading", { name: text })).toHaveClass(className);
-  });
+    test("[ 2 ] attribute 'className' sets rendered heading 'class' attribute", () => {
+        const text: string = "heading text";
+        const className: HeadingProps["className"] = "test-class";
+        const headingText: HeadingProps["children"] = text;
+
+        render(<Heading children={headingText} className={className} type={HEADING_TYPE.H1} />)
+        expect(screen.getByRole("heading", { name: text })).toHaveClass(className);
+    });
+
+    test("[ 3 ] attribute 'id' sets rendered heading 'id' attribute", () => {
+        const text: string = "heading text";
+        const headingId: HeadingProps["id"] = "test-id";
+        const headingText: HeadingProps["children"] = text;
+
+        render(<Heading children={headingText} id={headingId} type={HEADING_TYPE.H1} />)
+        expect(screen.getByRole("heading", { name: text })).toHaveAttribute("id", headingId);
+    });
 });
